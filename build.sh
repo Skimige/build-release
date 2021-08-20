5f9318f346e2f16e2dcf6e6a2a217d4122030027
Skimige <me@xyx.moe>
2021-08-20 22:58:59 +0800

    Use -j1 for building

--- build.sh
+++ build.sh
@@ -17,10 +17,10 @@ sleep 10
 
 mkdir .build_x
 echo CONFIG_VERSION_NUMBER=\"`cat release.tag`\" >.build_x/env
-./feeds/x/rom/lede/make.sh make -j$1 && sh upload.sh
+./feeds/x/rom/lede/make.sh make -j1 && sh upload.sh
 
 _EXIT=$?
 [ "x$_EXIT" = "x0" ] || {
-	make V=s >>../make.log 2>&1
+	make -j1 V=s >>../make.log 2>&1 && sh upload.sh
 	exit $_EXIT
 }
